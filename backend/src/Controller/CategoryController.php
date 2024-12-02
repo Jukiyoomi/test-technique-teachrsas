55f16d6f68e203fd17228586a4c921c5b9324fd1
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2024-12-02 17:50:09 +0100

    feat: create controller for category

---
+++ backend/src/Controller/CategoryController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\DTOs\CategoryDto;
+use App\Service\CategoryService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CategoryController extends AbstractController
+{
+	public function __construct(
+		private readonly CategoryService $categoryService
+	){}
+
+	#[Route('/category', name: 'api.category.create', methods: ['POST'], format: 'json')]
+	public function index(
+		#[MapRequestPayload] CategoryDto $categoryDto
+	): JsonResponse
+	{
+			$category = $this->categoryService->create($categoryDto);
+			return $this->json($category, Response::HTTP_CREATED);
+	}
+
+	#[Route('/category', name: 'api.category.find.all', methods: ['GET'])]
+	public function findAll(): JsonResponse
+	{
+		$categories = $this->categoryService->findAll();
+		return $this->json($categories);
+	}
+
+	#[Route('/category/{id}', name: 'api.category.find.one', requirements: ['id' => '\d+'], methods: ['GET'])]
+	public function findOne(int $id): JsonResponse
+	{
+		$category = $this->categoryService->findOneBy($id);
+		if (!$category) {
+			return $this->json([
+				'message' => "La catégorie d'id $id n'existe pas."
+			], Response::HTTP_NOT_FOUND);
+		}
+		return $this->json($category);
+	}
+
+	#[Route('/category/{id}', name: 'api.category.update', requirements: ['id' => '\d+'], methods: ['PUT'], format: 'json')]
+	public function update(
+		int $id,
+		#[MapRequestPayload] CategoryDto $categoryDto
+	): JsonResponse
+	{
+		$category = $this->categoryService->findOneBy($id);
+		if (!$category) {
+			return $this->json([
+				'message' => "La catégorie d'id $id n'existe pas."
+			], Response::HTTP_NOT_FOUND);
+		}
+		$category->setName($categoryDto->name);
+		$this->categoryService->update($category, $categoryDto);
+		return $this->json($category);
+	}
+
+	#[Route('/category/{id}', name: 'api.category.delete', requirements: ['id' => '\d+'], methods: ['DELETE'], format: 'json')]
+	public function delete(int $id): JsonResponse
+	{
+		$category = $this->categoryService->findOneBy($id);
+		$this->categoryService->delete($category);
+		return $this->json([], Response::HTTP_NO_CONTENT);
+	}
+}
