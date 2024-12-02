bf90938db3a1b85769158de2b61891522d077011
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2024-12-02 17:48:37 +0100

    feat: create custom dto for category

---
+++ backend/src/DTOs/CategoryDto.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DTOs;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class CategoryDto
+{
+ public function __construct(
+	 #[Assert\NotBlank(
+		 message: "Le nom de la catégorie est obligatoire."
+	 )]
+	 #[Assert\Length(
+		 min: 3,
+		 max: 100,
+		 minMessage: "Le nom de la catégorie doit contenir au moins {{ limit }} caractères.",
+		 maxMessage: "Le nom de la catégorie doit contenir au maximum {{ limit }} caractères."
+	 )]
+	 public readonly string $name
+ ){}
+}
