751e39241270bc2468b79d8182c44459861ef1a7
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2024-12-02 19:30:37 +0100

    feat: create dto, repository and service for product

---
+++ backend/src/Repository/ProductRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Product;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Product>
+ */
+class ProductRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Product::class);
+    }
+
+		public function create(Product $product): Product
+		{
+			$em = $this->getEntityManager();
+			$em->persist($product);
+			$em->flush();
+
+			return $product;
+		}
+
+	public function update(): void
+	{
+		$em = $this->getEntityManager();
+		$em->flush();
+	}
+
+	public function delete(Product $product): void
+	{
+		$em = $this->getEntityManager();
+		$em->remove($product);
+		$em->flush();
+	}
+
+    //    /**
+    //     * @return Product[] Returns an array of Product objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('p.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Product
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
