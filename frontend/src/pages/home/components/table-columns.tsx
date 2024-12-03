84f6da38246cb5f1eecc58b02a884a4359a82ec1
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2024-12-03 18:38:08 +0100

    feat: add actions drawer on table

--- frontend/src/pages/home/components/table-columns.tsx
+++ frontend/src/pages/home/components/table-columns.tsx
@@ -1,6 +1,16 @@
 import {ColumnDef} from "@tanstack/react-table"
 import {Product} from "#root/services/schema/product";
 import {LargeText, Paragraph} from "#root/components/ui/typography";
+import {MoreHorizontal} from "lucide-react";
+import {Button} from "#root/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuTrigger
+} from "#root/components/ui/dropdown-menu";
+import {toast} from "sonner";
 
 export const columns: ColumnDef<Product>[] = [
 	{
@@ -35,5 +45,33 @@ export const columns: ColumnDef<Product>[] = [
 			const category = String(row.getValue("category_name"))
 			return <div className="text-right">{category}</div>
 		}
+	},
+	{
+		id: "actions",
+		cell: ({ row }) => {
+			const product = row.original
+
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" className="h-8 w-8 p-0">
+							<span className="sr-only">Open menu</span>
+							<MoreHorizontal className="h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>Actions</DropdownMenuLabel>
+						<DropdownMenuItem
+							onClick={() => {
+								navigator.clipboard.writeText(product.name)
+									.then(() => toast.info("Le nom du produit a été copié !"))
+							}}
+						>
+							Copier le nom
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			)
+		},
 	},
 ]
