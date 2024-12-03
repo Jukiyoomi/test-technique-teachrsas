760a9588f7d599d060aec8277545f357f7388df1
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2024-12-03 18:37:37 +0100

    feat: add toast provider to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from "@tanstack/react-router";
 import {router} from "#root/router";
+import { Toaster } from "#root/components/ui/sonner";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <Toaster />
   </StrictMode>,
 )
