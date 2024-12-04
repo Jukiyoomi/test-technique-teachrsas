28d1eda975c681612c1394d3c5c1b032b5ab6e84
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2024-12-04 17:57:31 +0100

    feat: add dialog for editing product

--- frontend/src/pages/home/components/table-row-actions.tsx
+++ frontend/src/pages/home/components/table-row-actions.tsx
@@ -3,6 +3,7 @@ import {CopyIcon} from "lucide-react";
 import {toast} from "sonner";
 import {Product} from "#root/services/schema/product";
 import {DeleteProductDialog} from "#root/pages/home/components/delete-product-dialog";
+import {EditProductDialog} from "#root/pages/home/components/edit-product-dialog.tsx";
 
 type Props = {
 	product: Product
@@ -21,6 +22,7 @@ export function TableRowActions({ product }:Props) {
 			>
 				<CopyIcon className="h-4 w-4" />
 			</Button>
+			<EditProductDialog product={product} />
 			<DeleteProductDialog product={product} />
 		</div>
 	)
