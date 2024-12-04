7794d6a7cfbe440ebce0e39613701eda1387e6fd
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2024-12-04 10:16:31 +0100

    feat: update schema

--- frontend/src/services/schema/category.ts
+++ frontend/src/services/schema/category.ts
@@ -3,6 +3,11 @@ import {z} from "zod";
 export const getOneCategorySchema = z.object({
 	id: z.number(),
 	name: z.string(),
+	products: z.array(z.object({
+		id: z.number(),
+		name: z.string(),
+		price: z.number(),
+	}))
 })
 
 export const getCategoriesSchema = z.array(getOneCategorySchema)
